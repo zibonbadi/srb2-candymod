1c911843373493f162deed72c048bf8dccd819d6
zibonbadi <zibonbadi@users.noreply.github.com>
2024-08-06 01:36:37 +0200

    Add user-adjustable animation speed

--- L_CandyMod.lua
+++ L_CandyMod.lua
@@ -1,7 +1,7 @@
 -- Legal
 print("\n\82" ..
 	"\x83===================================================\n" ..
-	"CandyMod v1.0.1 (c) 2024 Zibon \"PixL\" Badi\n" ..
+	"CandyMod v1.0.2 (c) 2024 Zibon \"PixL\" Badi\n" ..
 	"This add-on is licensed under the GNU Affero GPLv3\n" ..
 	"<https://www.gnu.org/licenses/agpl-3.0.en.html>\n" ..
 	"===================================================\n"
@@ -9,19 +9,33 @@ print("\n\82" ..
 
 -- Rewire collectibles to use CTF sprites
 states[S_RING] = {
-        sprite = SPR_RRNG,
-        frame = FF_ANIMATE|A,
-        tics = -1,
-        action = nil,
-        var1 = 6,
-        var2 = 1,
-        nextstate = S_RING
+	sprite = SPR_RRNG,
+	frame = FF_ANIMATE|A,
+	tics = -1,
+	action = nil,
+	var1 = 6,
+	var2 = 1,
+	nextstate = S_RING
 }
 
 local function candymod_colorize(mo)
--- Pick one at random
-mo.color = P_RandomRange(1, #skincolors - 1)
+	-- Pick one at random
+	mo.color = P_RandomRange(1, #skincolors - 1)
+end
+
+local function candymod_adjustspeed(cvar)
+	if (cvar.changed == true ) then
+		states[S_RING].var2 = cvar.value
+	end
 end
 
 addHook("MobjSpawn", candymod_colorize, MT_RING)
 addHook("MobjSpawn", candymod_colorize, MT_FLINGRING)
+
+CV_RegisterVar({ "candymod_animspeed",
+					defaultValue = 1,
+					flags=CV_CALL,
+					PossibleValue = CV_Natural,
+					func = candymod_adjustspeed,
+					})
+
